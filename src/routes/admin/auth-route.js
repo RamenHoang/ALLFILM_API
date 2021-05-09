fa14cc77c43b51d42bded4e620b2576c2aef765b
Anh Nguyen <anhnguyenhoang321@gmail.com>
2021-05-09 18:52:49 +0700

    Can access Dashboard after login

--- src/routes/admin/auth-route.js
+++ src/routes/admin/auth-route.js
@@ -1,4 +1,3 @@
-/* eslint-disable max-len */
 const express = require('express');
 const { auth } = require('../../controllers/admin');
 
@@ -7,5 +6,7 @@ const route = express.Router();
 route.get('/login', auth.login);
 
 route.post('/login', auth.login);
+
+route.get('/dashboard', auth.dashboard);
 
 module.exports = route;
